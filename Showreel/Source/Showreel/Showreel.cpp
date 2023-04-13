41cc4a069d3aa7266e7bbd5e4b0887c3b389ad4d
Saif <103699437+PentaPixelsUE@users.noreply.github.com>
2023-04-13 13:42:07 +0100

    initial setup

---
+++ Showreel/Source/Showreel/Showreel.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Showreel.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Showreel, "Showreel" );
